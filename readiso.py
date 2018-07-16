d7d879bcf43411ce90d6548c598fcb59e75fe50f
Sophia Fondell <sfondell@redhat.com>
2018-07-16 17:09:10 -0400

    Extracting squashfs.img as iso file and attempting to mount

--- readiso.py
+++ readiso.py
@@ -5,6 +5,12 @@
 import pycdlib
 import argparse
 import os
+import shutil
+
+try:
+	from cStringIO import StringIO as BytesIO
+except ImportError:
+	from io import BytesIO
 
 def build_path(img_name):
 	'''A function to build the full path to the specified iso file'''
@@ -19,21 +25,36 @@ if __name__ == '__main__':
 	args = parser.parse_args()
 	path = build_path(args.image) # Is this necesary? Full path not needed if in cwd
 	
+	# DECLARE NEW ISO OBJECT AND INITIALIZE BY OPENING FILE SPECIFIED IN ARGS
 	iso = pycdlib.PyCdlib()
-	subdir = args.image + '/LIVEOS'
-	iso.open(subdir)
+	iso.open(args.image)
 
-	# for child in iso.list_children(iso_path='/LIVEOS'):
-	# 	print(str(child.file_identifier()))
+	# PRINT ALL FILE IDENTIFIERS IN ROOT DIRECTORY (DEBUGGGING PURPOSES)
+	for child in iso.list_children(iso_path='/'):
+		print(child.file_identifier())
 
-	# for child in iso.list_children(iso_path='/LIVEOS'):
-	# 	if ('SQUASHFS.IMG' in str(child.file_indentifier())):
-	# 		iso.open(child)
+	print()
 
-	# for child in iso.list_children(iso_path='/LIVEOS'):
-	# 	idf = str(child.file_identifier())
-	# 	if ('SQUASHFS.IMG' in idf):
-	# 		subdir_iso = pycdlib.PyCdlib()
-	# 		subdir_iso.open()
-	# 		for subchild in subdir_iso.list_children(iso_path='/'):
-	# 			print(subchild.file_identifier())
+	# PRINT ALL FILE IDENTIFIERS IN LIVEOS DIRECTORY (DEBUGGING PURPOSES)
+	for child in iso.list_children(iso_path='/LIVEOS'):
+		print(child.file_identifier())
+
+	# EXTRACT SQUASHFS.IMG FROM ISO AND THEN OPEN IT LOCALLY AND READ CHILDREN FILE IDENTIFIERS
+	extracted_img = BytesIO()
+	iso.get_file_from_iso_fp(extracted_img, iso_path='/LIVEOS/SQUASHFS.IMG;1')
+	
+	# WRITE BINARYIO OBJECT HOLDING SQUASHFS.IMG TO AN ISO IN CWD
+	extracted_img.seek(0)
+	with open('squashfs.img', 'wb') as f:
+		shutil.copyfileobj(extracted_img, f)
+
+	# # CLOSE ORIGINAL BOOT.ISO
+	# iso.close()
+
+	# # OPEN SQUASHFS.ISO
+	# iso = pycdlib.PyCdlib()
+	# iso.open('squashfs.iso')
+
+	# # PRINT ALL FILE IDENTIFIERS IN ROOT DIRECTORY OF SQUASHFS.ISO
+	# for child in iso.list_children(iso_path='/'):
+	# 	print(child.file_identifier())
