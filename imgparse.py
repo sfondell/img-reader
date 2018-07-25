382317f1403a473d5b28277fcb003d9c079e4689
Sophia Fondell <sfondell@redhat.com>
2018-07-25 14:35:55 -0400

    Functionality to print installed packages

--- imgparse.py
+++ imgparse.py
@@ -10,6 +10,7 @@ import argparse
 import os
 import shutil
 import glob
+import subprocess
 
 try:
 	from cStringIO import StringIO as BytesIO
@@ -70,6 +71,12 @@ def iter_fileglobs(rootdir, fileglobs):
 		for f in glob.glob(os.path.join(rootdir, fileglob.lstrip('/'))):
 			yield f
 
+def get_packages(rootdir):
+	'''A function to return all of the packages installed in a disk image'''
+	pkgs = subprocess.check_output(['rpm', '--root', '/mnt', '-qa'])
+	pkglist = pkgs.decode('utf-8').split('\\')
+	return pkglist
+
 def init_parser():
 	parser = argparse.ArgumentParser()
 	parser.add_argument('liveiso', help='filename of live image to be parsed')
@@ -79,6 +86,8 @@ def init_parser():
 					help='list all files in the image\'s rootfs image filesystem')
 	modes.add_argument('--ls', metavar='FILEGLOB', nargs='+',
 					help='list the given FILEGLOBS')
+	modes.add_argument('--list-pkgs', action='store_true',
+					help='list all packages installed in the image')
 
 	return parser
 
@@ -88,76 +97,21 @@ if __name__ == '__main__':
 
 	with MountedImage(args.liveiso) as live:
 		if args.list_all:
-			filenames = list(iter_files(live.rootfs))
+			imgdata = list(iter_files(live.rootfs))
 		elif args.ls:
-			filenames = list(iter_fileglobs(live.rootfs, args.ls))
+			imgdata = list(iter_fileglobs(live.rootfs, args.ls))
+		elif args.list_pkgs:
+			imgdata = get_packages(live.rootfs)
 		else:
-			assert('allegedly should never happen')
+			# This should never be reached because of the parser object
+			pass
 
-		if len(filenames) == 0:
-			parser.error('no files found')
+		if len(imgdata) == 0:
+			if (args.list_all or args.ls):
+				parser.error('no matching files found')
+			else:
+				parser.error('no matching packages found')
 
-		for f in filenames:
+		for f in imgdata:
 			outf = f[len(live.rootfs):]
 			print(outf)
-
-	# parser = argparse.ArgumentParser(description='Search a disk image\'s filesystem for a specified executable.')
-	# parser.add_argument('image', metavar='img', type=str, help='an image to be read')
-	# parser.add_argument('cmd', metavar='cmd', type=str, help='executable command/file to search for in iso file')
-	# args = parser.parse_args()
-	# path = build_path(args.image) # Is this necesary? Full path not needed if in cwd
-	
-	# # DECLARE NEW ISO OBJECT AND INITIALIZE BY OPENING FILE SPECIFIED IN ARGS
-	# iso = pycdlib.PyCdlib()
-	# iso.open(args.image)
-
-	# # PRINT ALL FILE IDENTIFIERS IN ROOT DIRECTORY (DEBUGGGING PURPOSES)
-	# for child in iso.list_children(iso_path='/'):
-	# 	print(child.file_identifier())
-
-	# print()
-
-	# # PRINT ALL FILE IDENTIFIERS IN LIVEOS DIRECTORY (DEBUGGING PURPOSES)
-	# for child in iso.list_children(iso_path='/LIVEOS'):
-	# 	print(child.file_identifier())
-
-	# # EXTRACT SQUASHFS.IMG FROM ISO AND THEN OPEN IT LOCALLY AND READ CHILDREN FILE IDENTIFIERS
-	# extracted_img = BytesIO()
-	# iso.get_file_from_iso_fp(extracted_img, iso_path='/LIVEOS/SQUASHFS.IMG;1')
-	
-	# # WRITE BINARYIO OBJECT HOLDING SQUASHFS.IMG TO AN ISO IN CWD
-	# extracted_img.seek(0)
-	# with open('squashfs.img', 'wb') as f:
-	# 	shutil.copyfileobj(extracted_img, f)
-
-	# # CLOSE ORIGINAL BOOT.ISO
-	# iso.close()
-
-	# # RUN COMMANDS TO MOUNT AS WE CAN'T OPEN SQUASHFS.IMG USING THIS LIBRARY
-	# os.system('sudo mount -o loop squashfs.img /mnt')
-
-	# # GET ORIGINAL DIRECTORY BEFORE WE CD AND MOVE ROOTFS.IMG BACK TO ORIGINAL WD
-	# owd = os.getcwd()
-	# os.chdir('/mnt/LiveOS/')
-	# copy_str = 'cp rootfs.img ' + owd
-	# os.system(copy_str)
-
-	# # SWITCH BACK TO ORIGINAL DIRECTORY TO REMOVE SQUASHFS.IMG FROM MOUNT & MOUNT ROOTFS.IMG
-	# os.chdir(owd)
-	# os.system('sudo umount /mnt')
-	# os.system('sudo mount -o loop rootfs.img /mnt')
-
-	# # CHANGE DIRECTORY TO /MNT SO WE CAN LOOK AT THE ISO'S FILESYSTEM
-	# os.chdir('/mnt')
-	# sbin = os.listdir('/usr/sbin')
-	# if (args.cmd in sbin):
-	# 	print('%s is present in the specified iso: %s' % (args.cmd, args.image))
-	# else:
-	# 	print('ERROR: %s is missing from the specified iso: %s' % (args.cmd, args.image))
-
-	# # UNMOUNT ROOTFS AND DELETE BOTH INTERMEDIATE IMG FILES
-	# os.chdir(owd)
-	# os.system('sudo umount /mnt')
-	# os.remove('squashfs.img')
-	# os.remove('rootfs.img')
-	
